0112df815d1298cb14f5bf9a2df13fb1628a7293
Epic Gyc <gyc8787@163.com>
2021-09-05 17:11:36 +0800

    temp upload

--- 6.15 new data/Environment.py
+++ 6.15 new data/Environment.py
@@ -6,56 +6,57 @@ import random
 class Environment():
     def __init__(self):
         super().__init__()
-        with open("./实验代码-new/DNN_Partition.csv", "r")as f:
-            self.array_data = np.loadtxt(f, delimiter=",")
+        tem = pd.read_csv("./6.15 new data/[2021.06.10]DNN_Partition.csv")
+        all_delay = self.partition_data = tem[tem['epoch']==1]
+        all_flop = self.flop_data = pd.read_csv("./6.15 new data/[2021.06.10]cuflopFlag.csv")
         self.actions = np.arange(0, 23)
-        states = []
-        rewards = []
-        for data in self.array_data:
-            states.append([data[2], data[3]])
-            rewards.append(data[6])
-        self.states = np.array(states)
-        self.rewards = np.array(rewards)
+        max_delay,min_delay = all_delay['allDelay'].max(),all_delay['allDelay'].min()
+        max_flop,min_flop = all_flop['S_flops'].max(),all_flop['S_flops'].min()
+        all_data = pd.DataFrame(columns=('gpu','batch','cut','reward'))
+        count = 0
+        for item in all_delay.iterrows():
+            [epoch,gpu,batch,cut,delay,_,_] = item[1].values
+            delay_reward = (delay-min_delay)/(max_delay-min_delay)
+            power = all_flop[(all_flop['batchNum']==batch) & (all_flop['modelCutID_T']==cut)]['S_flops'].mean()
+            power_reward = (power-min_flop)/(max_flop-min_flop)
+            all_data.loc[count] = {'gpu':gpu,'batch':batch,'cut':cut,'reward':delay_reward+power_reward}
+            count+=1
+        self.all_data = all_data
+    
 
-    def get_reward(self, state, action):
-        for i in range(8):
-            index = i*23+action
-            if((self.states[index] == state).all()):
-                return self.rewards[index]
-        return -1
-
-    def get_rewards(self, state):
-        for i in range(8):
-            index = i*23
-            if((self.states[index] == state).all()):
-                return self.rewards[index:index+23]
-        return []
+    def get_reward(self, state):
+        ''' state = [gpu,batch] '''
+        [gpu,batch] = state
+        data = self.data_frame
+        res = data[(data['gpu']==gpu)&(data['batch']==batch)]['reward'].values[0]
+        return res
+    
+    def get_reward_by_action(self,action):
+        data = self.all_data
+        reward = data[(data['cut']==action)]['reward'].mean()
+        return reward
 
     def best_step(self, state):
-        for i in range(8):
-            index = i*23
-            if((self.states[index] == state).all()):
-                rewards = self.rewards[index:index+23].tolist()
-                action = rewards.index(min(rewards))
-                return action, rewards[action]
+        
         return -1, 0
 
     def random_step(self):
-        index = random.randint(0, len(self.rewards))
-        reward = self.rewards[index]
-        action = index % 22
-        state = self.states[index]
-        return state, action, reward
+        index = random.randint(0, self.__state_size)
+        data = self.data_frame.loc[index]
+        return [int(data['gpu']),int(data['cut'])], data['reward']
 
     def random_state(self):
-        index = random.randint(0, len(self.states)-1)
-        return self.states[index]
+        index = random.randint(0, self.__state_size)
+        data = self.data_frame.loc[index]
+        return [data['gpu'],data['cut']]
 
     def action_size(self):
         return len(self.actions)
 
     def state_size(self):
-        return len(self.states[0])
+        return self.__state_size
 
     def action_space(self):
         return self.actions
+
+env = Environment()
